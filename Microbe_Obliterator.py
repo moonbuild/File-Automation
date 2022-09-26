cd877fe4a67dd3b794ead197890255664a822138
moonbuild <styooodnt@gmail.com>
2022-09-26 15:12:40 +0530

    fourth commit, renames file if already present

--- Microbe_Obliterator.py
+++ Microbe_Obliterator.py
@@ -2,51 +2,75 @@
 folder = "/home/space-man/Downloads/"
 documents = "/home/space-man/Documents/"
 #[space-man is just my username]
+"""
+CRONTAB
+*/5 * * * * python /home/space-man/Microbe_Obliterator.py
+"""
 import os
 from pathlib import Path
 import shutil
 
-## U can experiment better with the below code!
-### DONT FORGET TO REMOVE the next TWO lines of code when actually putting this to use.
-for i in range(10):
-    Path(folder+'snake🐍'+str(i)+'.py').touch()
+def join(a,b):
+    return os.path.join(a,b)
 
-def move(file_loc, path):
-    os.makedirs(path, exist_ok=True)
-    shutil.move(file_loc, path)
 
-def sort():
+
+def EVE(file_loc, dst_path):
+    try:
+        if not os.path.exists(dst_path):
+            os.mkdir(dst_path)
+        shutil.move(file_loc, dst_path)
+
+    except shutil.Error:
+        files = os.scandir(folder)
+        for file_name in files:
+            file_name = file_name.name
+            if os.path.isdir(join(folder, file_name)):
+                continue
+            src = join(folder, file_name)
+            i=0
+            while True:
+                base = os.path.basename(src)
+                name = f'-{i}'.join(os.path.splitext(base)) if i!=0 else base
+                dst_loc = join(dst_path, name)
+                if not os.path.exists(dst_loc):
+                    shutil.move(src, dst_loc)
+                    break
+                i+=1
+    
+
+def WallE():
     for file_name in os.listdir(folder):
 
-        file_loc = folder+file_name
+        file_loc = join(folder,file_name)
 
-        if os.path.isdir(os.path.join(folder, file_name)):
+        if os.path.isdir(file_loc):
             continue  
-
         if 'wall' in file_name:
-            move(file_loc ,folder+'wallpaper/')
+            EVE(file_loc ,join(folder+'wallpaper/'))
 
         if file_name.endswith('.py'):
-            move(file_loc, folder+'Python files/')
+            EVE(file_loc, join(folder,'Python files/'))
         
+        if file_name.endswith('.txt'):
+            EVE(file_loc, documents)
+
         elif file_name.endswith('.jpeg') or file_name.endswith('.png') or file_name.endswith('.jpg'):
-            move(file_loc, '/home/space-man/Pictures/')
+            EVE(file_loc, '/home/space-man/Pictures/')
         
-        elif file_name.endswith('.txt'):
-            move(file_loc, documents)
             
         elif file_name.endswith('.zip'):
-            move(file_loc, folder+'Zip files/')
+            EVE(file_loc, join(folder,'Zip files/'))
 
         elif file_name.endswith('pdf'):
-            move(file_loc, documents)
+            EVE(file_loc, documents)
 
         elif file_name.endswith('.csv'):
-            move(file_loc, documents+'CSV Files/')
+            EVE(file_loc, join(documents,'CSV Files/'))
 
         else:
-            move(file_loc, folder+'Other/')
+            EVE(file_loc, join(folder+'Other/'))
             
 
 if __name__ == '__main__':
-    sort()
+    WallE()
